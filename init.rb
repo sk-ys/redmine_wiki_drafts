876a7b1a22da0ebff2379804ef2d090debdc2bc7
sk-ys <yskrmt@gmail.com>
2025-02-24 21:48:22 +0900

    v0.0.2

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_wiki_drafts do
   name 'Redmine Wiki Drafts Plugin'
   author 'sk-ys'
   description 'This is a Redmine plugin that automatically saves the content being edited in the Wiki as a draft'
-  version '0.0.1'
+  version '0.0.2'
   url 'https://github.com/sk-ys/redmine_wiki_drafts'
   author_url 'https://github.com/sk-ys'
   settings default: { max_stored_slots: 5, auto_save_interval_seconds: 60 },
